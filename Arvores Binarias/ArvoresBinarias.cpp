3cda814611bec2c7a2ec2c521229cc52dd7e691a
Harllem Alves do Nascimento <54108636+tiduswr@users.noreply.github.com>
2021-09-21 12:00:09 -0300

    Implementação de Generics
    Foi implementado a função de Generics, para que a Arvore funcione com todos os tipos de dados sem que seja necessário realizar alteração nas funções da Arvore!

--- Arvores Binarias/ArvoresBinarias.cpp
+++ Arvores Binarias/ArvoresBinarias.cpp
@@ -1,121 +1,254 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 //Structs
-struct dados{
-    int key;
-};
+template<typename T, typename I>
 struct treeNode{
-    dados info;
-    treeNode *left, *right;
+    T info;
+    I key;
+    treeNode<T, I> *left, *right;
     int bal;
-    bool isAVL; // BSTree e 
+    bool isAVL; // BSTree e AVL
 };
 
-//Mude as funções abaixo caso a estrutura dados mude
-void printTreeNode(treeNode* node){
-    if(node != NULL) cout << node->info.key << " ";
+//A função abaixo pode dar erro caso I não seja numérico!
+template<typename T, typename I>
+int somarKeys(treeNode<T, I> *root){
+    if(root == NULL){
+        return 0;
+    }else{
+        return somarKeys(root->left) + somarKeys(root->right) + root->key;
+    }
 }
-int checkKey(dados valueOne, dados valueTwo){
+
+//Funções Principais
+template<typename T, typename I>
+void printTreeNode(treeNode<T, I> *node){
+    if(node != NULL) cout << node->key << " ";
+}
+template<typename I>
+int checkKey(I valueOne, I valueTwo){
     /* 0 é igual
     *  1 é maior
     *  -1 é menor*/
 
-    if(valueOne.key > valueTwo.key)return 1;
-    else if(valueOne.key < valueTwo.key) return -1;
-    else if(valueOne.key == valueTwo.key) return 0;
+    if(valueOne > valueTwo)return 1;
+    else if(valueOne < valueTwo) return -1;
+    else if(valueOne == valueTwo) return 0;
 }
-int somarKeys(treeNode* root){
-    if(root == NULL){
-        return 0;
-    }else{
-        return somarKeys(root->left) + somarKeys(root->right) + root->info.key;
-    }
-}
-//------------------------------------------------------------
-
-//Funções Principais
-void init(treeNode*& binaryTree){
+template<typename T, typename I>
+void init(treeNode<T, I> *&binaryTree){
     binaryTree = NULL;
 }
-void printPre(treeNode* root){
+template<typename T, typename I>
+void printPre(treeNode<T, I> *root){
     if(root != NULL){
         printTreeNode(root);
         printPre(root->left);
         printPre(root->right);
     }
 }
-void printCenter(treeNode* root){
+template<typename T, typename I>
+void printCenter(treeNode<T, I> *root){
     if(root != NULL){
         printCenter(root->left);
         printTreeNode(root);
         printCenter(root->right);
     }
 }
-void printPos(treeNode* root){
+template<typename T, typename I>
+void printPos(treeNode<T, I> *root){
     if(root != NULL){
         printPos(root->left);
         printPos(root->right);
         printTreeNode(root);
     }
 }
-bool insertBSTree(treeNode*& root, dados value){
+template<typename T, typename I>
+bool insertBSTree(treeNode<T, I> *&root, T value, I key){
     //A função retorna falso caso o valor ja exista na árvore
 
     if(root == NULL){
-        root = new treeNode;
+        root = new treeNode<T, I>;
         root->info = value;
-        root->left = NULL;
-        root->right = NULL;
+        root->key = key;
+        root->left = root->right = NULL;
         root->isAVL = false;
         return true;
     }
-    else if(checkKey(root->info, value) == 1) insertBSTree(root->left, value); 
-    else if(checkKey(root->info, value) == (-1)) insertBSTree(root->right, value);
-    else if(checkKey(root->info, value) == 0) return false;
+    else if(checkKey(root->key, key) == 1) return insertBSTree(root->left, value, key);
+    else if(checkKey(root->key, key) == -1) return insertBSTree(root->right, value, key);
+    else if(checkKey(root->key, key) == 0) return false;
 }
-treeNode* findNode(treeNode* root, dados value){
-    if(root == NULL || checkKey(root->info, value) == 0) return root;
-    else if(checkKey(root->info, value) == 1) return findNode(root->left, value);
-    else return findNode(root->right, value);
+template<typename T, typename I>
+treeNode<T, I> * findNode(treeNode<T, I> *root, I key){
+    if(root == NULL || checkKey(root->key, key) == 0) return root;
+    else if(checkKey(root->key, key) == 1) return findNode(root->left, key);
+    else return findNode(root->right, key);
 }
-int treeLenght(treeNode* root){
+template<typename T, typename I>
+int treeLenght(treeNode<T, I> *root){
     if(root == NULL){
         return 0;
     }else{
         return treeLenght(root->left) + treeLenght(root->right) + 1;
     }
 }
+template<typename T, typename I>
+int treeHeight(treeNode<T, I> *root){
+    if(root == NULL){
+        return -1;
+    }else{
+        int heightLeft = treeHeight(root->left);
+        int heightRight = treeHeight(root->right);
+        if(heightLeft > heightRight){
+            return heightLeft + 1;
+        }else{
+            return heightRight + 1;
+        }
+    }
+}
+template<typename T, typename I>
+treeNode<T, I>* treeMaxKeyValue(treeNode<T, I> *root){
+    if(root == NULL || root->right == NULL) return root;
+    else return treeMaxKeyValue(root->right);
+}
+template<typename T, typename I>
+treeNode<T, I>* treeMinKeyValue(treeNode<T, I> *root){
+    if(root == NULL || root->left == NULL) return root;
+    else return treeMinKeyValue(root->left);
+}
+template<typename T, typename I>
+treeNode<T, I>* removeTreeNode(treeNode<T, I> *root, I key){
+    //Procura o nó com o valor passado na função
+    if(root == NULL) return NULL;
+    else if(checkKey(root->key, key) == 1) root->left = removeTreeNode(root->left, key);
+    else if(checkKey(root->key, key) == -1) root->right = removeTreeNode(root->right, key);
+    else{
+        //Verifica qual o caso de exclusão
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            root = NULL;
+        }else if(root->left == NULL){
+            treeNode<T, I> * aux = root;
+            root = root->right;
+            delete aux;
+        }else if(root->right == NULL){
+            treeNode<T, I> *aux = root;
+            root = root->left;
+            delete aux;
+        }else{
+            //No caso em que possui duas folhas, é pegado primeiro a ramificação da esquerda
+            treeNode<T, I> *aux = root->left;
+            T value = root->info;
+            while(aux->right != NULL){
+                //Na ramificação da esquerda é feita um busca para achar o maior valor
+                aux = aux->right;
+            }
+            //Esse maior valor é copiado para a raiz atual, pois ele sera a nova raiz
+            root->info = aux->info;
+
+            //E então deletamos o valor desejado
+            delete aux;
+        }
+    }
+    return root;
+}
+template<typename T, typename I>
+bool isBalancedTree(treeNode<T, I> *root){
+    if(root == NULL) return true;
+    else{
+        if(abs(treeHeight(root->left) - treeHeight(root->right)) > 1) return false;
+        else{
+            return isBalancedTree(root->left) && isBalancedTree(root->right);
+        }
+    }
+}
+template<typename T, typename I>
+void generateVector(treeNode<T, I> *root, T *arrayData, I *arrayKeys, int &i){
+    if(root != NULL){
+        generateVector(root->left, arrayData, arrayKeys, i);
+        arrayData[i] = root->info;
+        arrayKeys[i] = root->key;
+        i++;
+        generateVector(root->right, arrayData, arrayKeys, i);
+    }
+}
+template<typename T, typename I>
+void killTree(treeNode<T, I> *&root){
+    if(root != NULL){
+        killTree(root->left);
+        killTree(root->right);
+        delete root;
+        root = NULL;
+    }
+}
+template<typename T, typename I>
+void makeBalancedTree(treeNode<T, I> *&root, T* arrayData, I* arrayKeys, int startArray, int endArray){
+    if(endArray < startArray){
+        root = NULL;
+    }else{
+        int MEIO = (startArray + endArray) / 2;
+        root = new treeNode<T, I>;
+        root->info = arrayData[MEIO];
+        root->key = arrayKeys[MEIO];
+        makeBalancedTree(root->left, arrayData, arrayKeys, startArray, MEIO-1);
+        makeBalancedTree(root->right, arrayData, arrayKeys, MEIO+1, endArray);
+    }
+}
+template<typename T, typename I>
+bool staticBalanceTree(treeNode<T, I> *&root){
+    if(!isBalancedTree(root)){
+        int lengthTree = treeLenght(root);
+        T* arrayData = new T[lengthTree];
+        I* arrayKeys = new I[lengthTree];
+        int i = 0;
+        generateVector(root, arrayData, arrayKeys, i);
+        killTree(root);
+        makeBalancedTree(root, arrayData, arrayKeys, 0, lengthTree - 1);
+        delete [] arrayData;
+        delete [] arrayKeys;
+        return true;
+    }else{
+        return false;
+    }
+}
 
 //--------------------------------------------------------------------------------------------------
 //Código para Arvores Avl
-bool isAvlTree(treeNode* root){
+template<typename T, typename I>
+bool isAvlTree(treeNode<T, I> *root){
     return root->isAVL;
 }
-void rotateRightAVL(treeNode*& root){
-    treeNode* aux;
+template<typename T, typename I>
+void rotateRightAVL(treeNode<T, I> *&root){
+    treeNode<T, I> *aux;
     aux = root->left;
     root->left = aux->right;
     aux->right = root;
     root = aux;
 }
-void rotateLeftAVL(treeNode*& root){
-    treeNode* aux;
+template<typename T, typename I>
+void rotateLeftAVL(treeNode<T, I> *&root){
+    treeNode<T, I> *aux;
     aux = root->right;
     root->right = aux->left;
     aux->left = root;
     root = aux;
 }
-bool insertAvlTree(treeNode* root, dados value){
+template<typename T, typename I>
+bool insertAvlTree(treeNode<T, I> *root, T value, I key){
     if(root == NULL){
-        root = new treeNode;
+        root = new treeNode<T, I>;
         root->info = value;
         root->left = root->right = NULL;
+        root->key = key;
         root->bal = 0;
         return true;
     }else{
-        if(checkKey(root->info, value) == 1){
+        if(checkKey(root->key, key) == 1){
             if(insertAvlTree(root->left, value)){
                 switch(root->bal){
                     case +1: root->bal = 0; return false;
@@ -132,7 +265,7 @@ bool insertAvlTree(treeNode* root, dados value){
                 }
             }
         }else{
-            if(checkKey(value, root->info) == 1){
+            if(checkKey(key, root->key) == 1){
                 switch(root->bal){
                     case -1: root->bal = 0; return false;
                     case 0: root->bal = +1; return true;
@@ -151,123 +284,17 @@ bool insertAvlTree(treeNode* root, dados value){
 }
 //----------------------------------------------------------------------------------------------------
 
-int treeHeight(treeNode* root){
-    if(root == NULL){
-        return -1;
-    }else{
-        int heightLeft = treeHeight(root->left);
-        int heightRight = treeHeight(root->right);
-        if(heightLeft > heightRight){
-            return heightLeft + 1;
-        }else{
-            return heightRight + 1;
-        }
-    }
-}
-treeNode* treeMaxKeyValue(treeNode* root){
-    if(root == NULL || root->right == NULL) return root;
-    else return treeMaxKeyValue(root->right);
-}
-treeNode* treeMinKeyValue(treeNode* root){
-    if(root == NULL || root->left == NULL) return root;
-    else return treeMinKeyValue(root->left);
-}
-treeNode* removeTreeNode(treeNode* root, dados value){
-    //Procura o nó com o valor passado na função
-    if(root == NULL) return NULL;
-    else if(checkKey(root->info, value) == 1) root->left = removeTreeNode(root->left, value);
-    else if(checkKey(root->info, value) == -1) root->right = removeTreeNode(root->right, value);
-    else{
-        //Verifica qual o caso de exclusão
-        if(root->left == NULL && root->right == NULL){
-            delete root;
-            root = NULL;
-        }else if(root->left == NULL){
-            treeNode * aux = root;
-            root = root->right;
-            delete aux;
-        }else if(root->right == NULL){
-            treeNode* aux = root;
-            root = root->left;
-            delete aux;
-        }else{
-            //No caso em que possui duas folhas, é pegado primeiro a ramificação da esquerda
-            treeNode* aux = root->left;
-            while(aux->right != NULL){
-                //Na ramificação da esquerda é feita um busca para achar o maior valor
-                aux = aux->right;
-            }
-            //Esse maior valor é copiado para a raiz atual, pois ele sera a nova raiz
-            root->info = aux->info;
-            //A raiz que possuia o maior valor é então substituida pelo valor que queremos excluir
-            aux->info = value;
-            //Então pegamos a ramificação da esquerda e excluimos efetivamente o valor procurado
-            root->left = removeTreeNode(root->left, value);
-        }
-    }
-    return root;
-}
-bool isBalancedTree(treeNode* root){
-    if(root == NULL) return true;
-    else{
-        if(abs(treeHeight(root->left) - treeHeight(root->right)) > 1) return false;
-        else{
-            return isBalancedTree(root->left) && isBalancedTree(root->right);
-        }
-    }
-}
-void generateVector(treeNode* root, dados* array, int &i){
-    if(root != NULL){
-        generateVector(root->left, array, i);
-        array[i] = root->info;
-        i++;
-        generateVector(root->right, array, i);
-    }
-}
-void killTree(treeNode *&root){
-    if(root != NULL){
-        killTree(root->left);
-        killTree(root->right);
-        delete root;
-        root = NULL;
-    }
-}
-void makeBalancedTree(treeNode*& root, dados* array, int startArray, int endArray){
-    if(endArray < startArray){
-        root = NULL;
-    }else{
-        int MEIO = (startArray + endArray) / 2;
-        root = new treeNode;
-        root->info = array[MEIO];
-        makeBalancedTree(root->left, array, startArray, MEIO-1);
-        makeBalancedTree(root->right, array, MEIO+1, endArray);
-    }
-}
-bool staticBalanceTree(treeNode *&root){
-    if(!isBalancedTree(root)){
-        int lengthTree = treeLenght(root);
-        dados* array = new dados[lengthTree];
-        int i = 0;
-        generateVector(root, array, i);
-        killTree(root);
-        makeBalancedTree(root, array, 0, lengthTree - 1);
-        delete [] array;
-        return true;
-    }else{
-        return false;
-    }
-}
 int main(){
 
-    treeNode *tree, *aux;
+    treeNode<string, int> *tree, *aux;
     int NUMEROS[] = {1,2,3,4,5,6,7,8,9,10,11};
-    dados *TESTE = new dados[5];
+    string *TESTE = new string[5];
     int TAMANHO_ARRAY_NUMEROS = sizeof(NUMEROS)/sizeof(int);
     init(tree);
 
     cout << "Inserindo valores do array NUMEROS..." << endl;
     for(int i = 0; i < TAMANHO_ARRAY_NUMEROS; i++){
-        if(!insertBSTree(tree, dados{NUMEROS[i]})){
+        if(!insertBSTree(tree, to_string(NUMEROS[i]), NUMEROS[i])){
             cout << "O elemento " << NUMEROS[i] << " já existe!" << endl;
         }
     }
@@ -280,9 +307,9 @@ int main(){
     printPos(tree);
     cout << endl;
 
-    aux = findNode(tree, dados{2});
+    aux = findNode(tree, 2);
     if(aux != NULL){
-        cout << "Teste de Busca: " << aux->info.key;
+        cout << "Teste de Busca: " << aux->key;
     }else{
         cout << "O valor de Busca não foi encontrado!";
     }
@@ -299,7 +326,7 @@ int main(){
 
     aux = treeMaxKeyValue(tree);
     if(aux != NULL){
-        cout << "Maior valor da Arvore: " << aux->info.key;
+        cout << "Maior valor da Arvore: " << aux->key;
     }else{
         cout << "Não foi encontrado um valor máximo!";
     }
@@ -307,22 +334,27 @@ int main(){
 
     aux = treeMinKeyValue(tree);
     if(aux != NULL){
-        cout << "Menor valor da Arvore: " << aux->info.key;
+        cout << "Menor valor da Arvore: " << aux->key;
     }else{
         cout << "Não foi encontrado um valor minimo!";
     }
     cout << endl;
 
     cout << "Removendo o valor 11" << endl;
-    removeTreeNode(tree, dados{11});
+    removeTreeNode(tree, 11);
     printCenter(tree);
+    cout << endl;
+    printPre(tree);
     cout<< endl;
     if(isBalancedTree(tree)) cout << "Arvore Balanceda";
     else cout << "Arvore Desbalanceada! Iniciando balanceamento estatico...";
     cout << endl;
     staticBalanceTree(tree);
     cout << "A arvore Balanceada é: ";
+    cout << endl;
     printPre(tree);
+    cout << endl;
+    printCenter(tree);
     cout << endl;
 
     cout << endl << endl << "Fim da Execução..." << endl;
